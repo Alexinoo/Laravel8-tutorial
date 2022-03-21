f532f7a108070d4d49f8a641ad20291aacc25812
Alex Mwangi <alex@stl-horizon.com>
2022-03-21 15:39:46 +0300

    Add routes with required parameters

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+// Required Parameter
+Route::get('/users/{name}', function ($name) {
+
+    return 'Hi ' . $name;
+});
