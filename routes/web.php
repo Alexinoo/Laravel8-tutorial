d8370010319827c50d4cb44c26aa1ff781825a44
Alex Mwangi <alex@stl-horizon.com>
2022-03-21 16:19:52 +0300

    Configured route for edit method

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,5 @@ Route::get('/', function () {
 
 
 Route::get('home', [App\Http\Controllers\HomeController::class, 'index']);
+
+Route::get('edit/{id}', [App\Http\Controllers\HomeController::class, 'edit']);
