d47bbe119cdd9669d4c7203038f18eead49c4bcb
Alex Mwangi <alex@stl-horizon.com>
2022-03-22 10:49:19 +0300

    Added index method and passed array data to welcome view

--- app/Http/Controllers/ProductCOntroller.php
+++ app/Http/Controllers/ProductCOntroller.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+        $fruits = array('Mango', 'Orange', 'Banana', 'Apple', 'Pineapple');
+
+        return view('welcome', [
+            'fruits' => $fruits
+        ]);
+    }
 }
