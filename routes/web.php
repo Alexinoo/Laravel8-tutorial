2baf77cc87804f63c8281073e9de81d91951193a
Alex Mwangi <alex@stl-horizon.com>
2022-03-22 11:39:39 +0300

    Configured route to call getAllPost from ClientController

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,23 @@ Route::get('home', [App\Http\Controllers\HomeController::class, 'index']);
 Route::get('edit/{id}', [App\Http\Controllers\HomeController::class, 'edit']);
 
 Route::get('user', [App\Http\Controllers\UserController::class, 'index']);
+
+
+
+// Http Client
+// -----------------
+
+
+// Http Client - Fetch Post
+Route::get('/posts', [App\Http\Controllers\ClientController::class, 'getAllPosts']);
+// Http Client - Fetch SIngle Post BY ID
+Route::get('/post/{id}', [App\Http\Controllers\ClientController::class, 'getPostById']);
+
+// Http Client - Add Post
+Route::get('/add-post ', [App\Http\Controllers\ClientController::class, 'addPost']);
+
+// Http Client - Update Post
+Route::get('/update-post ', [App\Http\Controllers\ClientController::class, 'updatePost']);
+
+// Http Client - Delete Post
+Route::get('/delete-post/{id}', [App\Http\Controllers\ClientController::class, 'deletePost']);
