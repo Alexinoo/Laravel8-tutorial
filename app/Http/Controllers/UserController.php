4951130adcb2c8986a5ba125db9b5ea657544b24
Alex Mwangi <alex@stl-horizon.com>
2022-03-22 20:45:49 +0300

    Added getMethod/getPath/getUrl/getFullUrl - Returns specific data

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,4 +19,26 @@ class UserController extends Controller
 
         return view('user', compact('name', 'userDetails'));
     }
+
+    public function getMethod(Request $request)
+    {
+
+        return $request->method(); //returns GET
+    }
+    public function getPath(Request $request)
+    {
+
+        return $request->path(); //return getPath
+    }
+    public function getUrl(Request $request)
+    {
+
+        return $request->url(); //return http://127.0.0.1:8000/getUrl - NO PARAMETERS
+    }
+
+    public function getFullUrl(Request $request)
+    {
+
+        return $request->fullUrl(); //return http://127.0.0.1:8000/getFullUrl?name=Alex - WITH Parameters
+    }
 }
