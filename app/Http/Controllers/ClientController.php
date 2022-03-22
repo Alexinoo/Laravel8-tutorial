2d46cc126e09b7b4f5cce3b90b0d740e48a5298e
Alex Mwangi <alex@stl-horizon.com>
2022-03-22 11:38:43 +0300

    Added getALlposts mthd that reurns all posts from a JSON placeholder FAKE API

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ClientController extends Controller
 {
-    //
+    public function getAllPosts()
+    {
+        $url = 'https://jsonplaceholder.typicode.com/posts';
+
+        $response = Http::get($url);
+
+        return $response->json();
+    }
 }
