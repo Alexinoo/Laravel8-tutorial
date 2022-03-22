f4acf569d2c86169e236f5305d59bcdb1ce5cab5
Alex Mwangi <alex@stl-horizon.com>
2022-03-22 11:42:25 +0300

    Added a route that invoke getPostById from the ClientController

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,6 @@ Route::get('user', [App\Http\Controllers\UserController::class, 'index']);
 
 // Http Client - Fetch Post
 Route::get('/posts', [App\Http\Controllers\ClientController::class, 'getAllPosts']);
+
 // Http Client - Fetch SIngle Post BY ID
 Route::get('/post/{id}', [App\Http\Controllers\ClientController::class, 'getPostById']);
-
-// Http Client - Add Post
-Route::get('/add-post ', [App\Http\Controllers\ClientController::class, 'addPost']);
-
-// Http Client - Update Post
-Route::get('/update-post ', [App\Http\Controllers\ClientController::class, 'updatePost']);
-
-// Http Client - Delete Post
-Route::get('/delete-post/{id}', [App\Http\Controllers\ClientController::class, 'deletePost']);
