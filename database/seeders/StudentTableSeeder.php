80c159b46211a810e97129f0938344a79bcec79f
Alex Mwangi <alex@stl-horizon.com>
2022-03-23 22:29:24 +0300

    Using Faker class to generate multiple reocords

--- database/seeders/StudentTableSeeder.php
+++ database/seeders/StudentTableSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class StudentTableSeeder extends Seeder
 {
@@ -14,10 +15,27 @@ class StudentTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('students')->insert([
-            'regNo' => 'BIT-001-4164/2010',
-            'name' => 'ALEX MWANGI WAKANYI',
-            'course' => 'BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY'
-        ]);
+
+        //INSERT MULTIPLE RECORDS
+
+        $faker = Faker::create();
+
+        foreach (range(1, 100)  as $index) {
+
+            DB::table('students')->insert([
+                'regNo' => $faker->sentence(17),
+                'name' => $faker->sentence(15),
+                'course' => $faker->paragraph(1)
+            ]);
+        }
+
+        //INSERT SINGLE RECORD
+
+        // DB::table('students')->insert([
+        //     'regNo' => 'BIT-001-4164/2010',
+        //     'name' => 'ALEX MWANGI WAKANYI',
+        //     'course' => 'BACHELOR OF SCIENCE IN INFORMATION TECHNOLOGY'
+        // ]);
+
     }
 }
