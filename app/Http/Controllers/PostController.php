acc02d44c45a892d6787771c681364c019483574
Alex Mwangi <alex@stl-horizon.com>
2022-03-23 20:28:27 +0300

    Modified rendering routes to point to Post directory

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-   
+    public function getAllPosts()
+    {
+
+        $posts = DB::table('posts')->get();
+
+        return view('Post.index', compact('posts'));
+    }
+
+    public function addPost()
+    {
+        return view('Post.create');
+    }
+
+    public function storePost(Request $request)
+    {
+        DB::table('posts')->insert([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return back()->with('post_created', 'Post has been created successfully');
+    }
 }
