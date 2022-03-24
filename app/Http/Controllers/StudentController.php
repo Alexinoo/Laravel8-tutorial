e5f28d6be08715d0bed596a58a9928af61efaa26
Alex Mwangi <alex@stl-horizon.com>
2022-03-24 21:48:43 +0300

    Added CRUD functionality

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -10,10 +10,69 @@ class StudentController extends Controller
 
     public function index()
     {
-        $students  = Student::paginate(7);
+        $students  = Student::orderBy('created_at', 'DESC')->paginate(7);
 
         return view('Student.index', [
             'students' => $students
         ]);
+    }
+
+    public function create()
+    {
+        return view('Student.create');
+    }
+
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'regNo' => 'required',
+            'name' => 'required',
+            'course' => 'required',
+        ]);
+
+        if ($validatedData) {
+
+            $student = new Student;
+            $student->regNo = $validatedData['regNo'];
+            $student->name = $validatedData['name'];
+            $student->course = $validatedData['course'];
+            $student->save();
+
+            return redirect('students')->with('status', 'Student Added Successfully');
+        }
+    }
+
+    public function edit($id)
+    {
+        $student =  Student::find($id);
+        return view('Student.edit', compact('student'));
+    }
+
+
+    public function update(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'regNo' => 'required',
+            'name' => 'required',
+            'course' => 'required',
+        ]);
+
+        if ($validatedData) {
+
+            $student =  Student::find($id);
+            $student->regNo = $validatedData['regNo'];
+            $student->name = $validatedData['name'];
+            $student->course = $validatedData['course'];
+            $student->update();
+
+            return redirect('students')->with('status', 'Student Updated Successfully');
+        }
+    }
+
+    public function destroy($id)
+    {
+        $student =  Student::find($id);
+        $student->delete();
+        return redirect('students')->with('status', 'Student Deleted Successfully');
     }
 }
