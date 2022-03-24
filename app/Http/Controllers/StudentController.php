59c3fbdb5290ff9e2a538de1c3f5c9e38ca22a7f
Alex Mwangi <alex@stl-horizon.com>
2022-03-24 14:50:18 +0300

    Added index() to render students in a list of 10 per page

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Student;
 
 class StudentController extends Controller
 {
-    //
+
+    public function index()
+    {
+        $students  = Student::paginate(10);
+
+        return view('Student.index', [
+            'students' => $students
+        ]);
+    }
 }
