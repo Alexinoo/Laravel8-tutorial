c5f0eeb88b926fdedca4410aa052efc59dd35237
Alex Mwangi <alex@stl-horizon.com>
2022-03-24 15:08:53 +0300

    Modified to use for loop

--- resources/views/Student/index.blade.php
+++ resources/views/Student/index.blade.php
@@ -6,33 +6,43 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-8">
-           <table class="table table-striped">
-               <thead>
-                   <tr>
-                       <th>#</th>
-                       <th>Reg No</th>
-                       <th>Name</th>
-                       <th>Course</th>
-                       <th>Operations</th>
-                   </tr>
-               </thead>
-               <tbody>
-                   @foreach($students as $key => $value)                       
-                   <tr>
-                       <td>{{$value->id}}</td>
-                       <td>{{$value->regNo}}</td>
-                       <td>{{$value->name}}</td>
-                       <td>{{$value->course}}</td>
-                       <td>
-                           <a href="{{ url('edit-student /'.$value->id)}}" class = "btn btn-primary btn-sm">Edit</a>
-                           <a href="{{ url('delete-student/'.$value->id)}}" class = "btn btn-danger btn-sm">Delete</a>
-                       </td>
-                    </tr>
-                    @endforeach
-               </tbody>
-           </table>
-           {{ $students->links() }}
+        <div class="col-md-12">
+            <div class="card my-3">
+                <div class="card-header text-center">
+                    <h4>Students</h4>
+                </div>
+                <div class="card-body">
+                      <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Reg No</th>
+                                    <th>Name</th>
+                                    <th>Course</th>
+                                    <th>Operations</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                @for($i = 1; $i < count($students); $i++)
+                                      <tr>
+                                            <td>{{$i}}</td>
+                                            <td>{{$students[$i ] ['regNo']}}</td>
+                                            <td>{{$students[$i ]['name'] }}</td>
+                                            <td>{{$students[$i ]['course'] }}</td>
+                                            <td>
+                                                <a href="{{ url('edit-student /'.$students[$i ] ['id'])}}" class = "btn btn-primary btn-sm">Edit</a>
+                                                <a href="{{ url('delete-student/'.$students[$i ] ['id'])}}" class = "btn btn-danger btn-sm">Delete</a>
+                                            </td>
+                                    </tr>
+                                @endfor                              
+                            </tbody>
+                        </table>
+                    </div>
+                </div>    
+                <div class="float-end">
+                    {{ $students->links() }}
+                    </div>    
         </div>
     </div>
 </div>
