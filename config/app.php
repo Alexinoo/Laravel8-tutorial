e2e2b8bb05c602875a4a3f7573321d8cb8f8d14b
Alex Mwangi <alex@stl-horizon.com>
2022-03-24 16:26:12 +0300

    Registered PaymentServiceProvider in the providers section; Registered Payment Facades in the aliases

--- config/app.php
+++ config/app.php
@@ -162,6 +162,9 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        // Register Service Provider
+        App\Providers\PaymentServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
@@ -229,6 +232,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // Register Payment Facades in the aliases
+        'Payment' => App\PaymentGateway\PaymentFacade::class,
 
     ],
 
