080b03b6f3d6d1e685c60562ea33e31f02c1bb0e
Alex Mwangi <alex@stl-horizon.com>
2022-03-25 15:20:37 +0300

    Added SendMail() that calls the TestMail class and pass the content

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\TestMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-   
+
+    public function SendEmail()
+    {
+        $content = [
+            'title' => 'Mail from Coding Pro',
+            'body' => 'Testing mail using Laravel 8',
+        ];
+
+        $vSend =   new TestMail($content);
+
+        Mail::to('mwangialex26@gmail.com')->send($vSend);
+
+        return "Email Sent successfully";
+    }
 }
