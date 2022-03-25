f0a1f7e67e4eadb80e3d66fa79d70ae62a08a903
Alex Mwangi <alex@stl-horizon.com>
2022-03-25 11:44:43 +0300

    Added phone() that defines a OneToOne rship with phone Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //ADD PARENT TO CHILD RELATIONSHIP
+    public function phone()
+    {
+        return  $this->hasOne('App\Models\Phone');
+    }
 }
