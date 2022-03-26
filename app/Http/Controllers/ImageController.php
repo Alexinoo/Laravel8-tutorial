d74dbfa100b10bd6de9739a49a822ba8c56ed361
Alex Mwangi <alex@stl-horizon.com>
2022-03-26 20:24:20 +0300

    Created ImageController; Added resizeImage -for file upload ; Added upload() to resize and store the image in the public/images folder

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Image;
+
+class ImageController extends Controller
+{
+    public function resizeImage()
+    {
+
+        return view('Image.resize-image');
+    }
+
+    public function upload(Request $request)
+    {
+
+        $image = $request->file('image');
+        $filename =  $image->getClientOriginalName();
+        $image_resize = Image::make($image->getRealpath());
+        $image_resize->resize(300, 300);
+        $image_resize->save(public_path('uploads/images/' . $filename));
+        return "Image has been resized successfully";
+    }
+}
