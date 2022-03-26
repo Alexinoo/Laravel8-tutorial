52640000aff1f17b8d99ea93467ec65388dc8bba
Alex Mwangi <alex@stl-horizon.com>
2022-03-26 21:02:43 +0300

    Added Dropzone menu

--- resources/views/layouts/inc/navbar.blade.php
+++ resources/views/layouts/inc/navbar.blade.php
@@ -24,6 +24,7 @@
             <li><a class="dropdown-item" href="{{ url('upload')}}">File Upload</a></li>        
             <li><a class="dropdown-item" href="{{ url('employees')}}">Employees</a></li>        
             <li><a class="dropdown-item" href="{{ url('resize-image')}}">Resize Image</a></li>        
+            <li><a class="dropdown-item" href="{{ url('dropzone')}}">DropZone</a></li>        
           </ul>
         </li>
           <li class="nav-item">
