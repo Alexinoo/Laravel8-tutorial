dcba539d5a80099f302af5df480e31d8337afba9
Alex Mwangi <alex@stl-horizon.com>
2022-03-26 23:34:29 +0300

    Added searchProduct() to render search products form ;  autocomplete(Request ) to return data that matches request->terms in json

--- app/Http/Controllers/ProductCOntroller.php
+++ app/Http/Controllers/ProductCOntroller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,5 +14,32 @@ class ProductController extends Controller
         return view('welcome', [
             'fruits' => $fruits
         ]);
+    }
+    public function addProduct()
+    {
+        $products = [
+            ['id' => 2, 'name' => 'Phone'],
+            ['id' => 3,  'name' =>  'Tablet'],
+            ['id' => 4, 'name' =>  'Laptops'],
+            ['id' => 5,  'name' =>  'Computer'],
+            ['id' => 6,   'name' =>  'Printer']
+        ];
+
+        Product::insert($products);
+
+        return "Product has been inserted successfully";
+    }
+
+    public function searchProduct()
+    {
+        return view('Product.search');
+    }
+
+    public function autocomplete(Request $request)
+    {
+        $data = Product::select('name')
+            ->where('name', 'LIKE', "%{$request->terms}%")
+            ->get();
+        return response()->json($data);
     }
 }
