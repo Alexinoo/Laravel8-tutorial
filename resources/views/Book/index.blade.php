e4bf07cd4ac26a817e970c9066b6845e7c655e7f
Alex Mwangi <alex@stl-horizon.com>
2022-03-27 15:12:53 +0300

    Added insert data without Ajax reload using modal;

---
+++ resources/views/Book/index.blade.php
@@ -0,0 +1,165 @@
+@extends('layouts.master')
+
+@section('title', 'AJAX CRUD')
+
+@section('content')
+
+<!-- Modal -->
+<div class="modal fade" id="bookModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Add New Book</h5>     
+      </div>
+      <div class="modal-body">
+              <ul id="errList"></ul>
+           <div class="form-group mb-3">
+               <label for="">ISBN</label>
+               <input type="text"  class="form-control isbn"  name="isbn"/>
+               <span id="errorIsbn"></span>
+           </div>
+           <div class="form-group mb-3">
+               <label for="">Title</label>
+               <input type="text"  class="form-control title"  name="title"/>
+                <span id="errorTitle"></span>
+           </div>
+           <div class="form-group mb-3">
+               <label for="">Author</label>
+               <input type="text"  class="form-control author"  name="author" />
+                <span id="errorAuthor"></span>
+           </div>
+           <div class="modal-footer mb-3">
+                 <button type="button" class="btn btn-secondary  close_add_book " data-bs-dismiss="modal">Close</button>
+                 <button type="button" class="btn btn-primary  add_book">Save</button>
+           </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="successMsg" class="mt-3"></div>
+            <div class="card my-3">
+                <div class="card-header text-center">
+                    <h4>Books
+                        <a class="btn btn-info float-end" data-bs-toggle="modal" data-bs-target="#bookModal">Add Book</a>
+                    </h4>
+                </div>
+                <div class="card-body">                   
+                      <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Isbn</th>
+                                    <th>Title</th>                                    
+                                    <th>Author</th>                                    
+                                    <th>Operations</th>                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach($books as $key => $value)
+                                    <tr>
+                                        <td>{{ $value->id}}</td>
+                                        <td>{{ $value->isbn}}</td>
+                                        <td>{{ $value->title}}</td>
+                                        <td>{{ $value->author}}</td>
+                                        <td>
+                                            <a href="{{ url('edit-book/'.$value->id)}}" class="btn btn-primary btn-sm">Edit</a>
+                                            <a href="{{ url('delete-book/'.$value->id)}}" class="btn btn-danger btn-sm">Delete</a>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                            
+                            </tbody>
+                        </table>
+                    </div>
+                </div>    
+         </div>
+    </div>
+</div>
+    
+@endsection
+
+
+@section('scripts')
+<script>
+    $(document).ready(function(){
+
+        // Grab the form
+        $(document).on('click','.add_book',function(e){
+            e.preventDefault();
+
+            let data = {
+                "isbn" : $('.isbn').val(),
+                "title" : $('.title').val(),
+                "author" : $('.author').val(),
+            }
+
+              $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+
+            $.ajax({
+                url : "/books" ,
+                type  : 'POST',
+                data : data ,
+                dataType: "json",
+                success : function(response){
+
+                    if(response.status == 404){       
+
+                    //LOOP THROUGH THE ERROR LIST - Append output to <ul></ul>
+                    // $.each(response.errors , function(index , error){
+                    //      $('#errList').append(`<li>${error}</li>`);
+                    // });    
+                    
+                    $('span').empty();       
+            
+                    //ERRORS ON INDIVIDUAL FIELD
+                    if(response.errors.isbn){                           
+                            $('#errorIsbn').addClass("text-danger");
+                            $('#errorIsbn').append(`<p>${response.errors.isbn}</p>`);
+                    }                      
+                    if(response.errors.title){    
+                            $('#errorTitle').addClass("text-danger");
+                            $('#errorTitle').append(`<p>${response.errors.title}</p>`);
+                    } 
+                    if(response.errors.isbn){ 
+                            $('#errorAuthor').addClass("text-danger");
+                            $('#errorAuthor').append(`<p>${response.errors.author}</p>`);
+                    } 
+                    
+                   
+                    }else{
+                          //EMPTY THE ERROR LIST
+                           $('span').empty();                 
+                           $('#successMsg').addClass('alert alert-success');
+                           $('#successMsg').text(response.message).fadeOut(4000); //Fade out after 4 sec
+
+                           //HIDE MODAL
+                           $('#bookModal').modal('hide');
+                           //EMPTY THE INPUT FIELDS
+                           $('#bookModal').find('input').val("");
+
+                    }
+                  
+                }
+            });
+            
+        });
+
+        $(document).on('click','.close_add_book',function(e){
+            e.preventDefault();
+             //REMOVES <p>errors</p>
+             $('span').empty();                    
+        })
+
+    });
+
+</script>
+    
+@endsection
