5bac5ede4a1993e0d6f499dc1e9e081ac5d2d979
Alex Mwangi <alex@stl-horizon.com>
2022-03-27 15:10:21 +0300

    Created BookController ; Added index for Books view ; Added store to add/save a book

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::orderBy('created_at', 'DESC')->get();
+        return view('Book.index', compact('books'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'isbn' => 'required',
+            'title' => 'required',
+            'author' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+
+            return response()->json([
+                'status' => 404,
+                'errors' => $validator->messages(),
+            ]);
+        } else {
+
+            $book = new Book;
+            $book->isbn = $request->input('isbn');
+            $book->title = $request->input('title');
+            $book->author = $request->input('author');
+            $book->save();
+
+            return response()->json([
+                'status' => 200,
+                'message' => 'Student Added successfully',
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
