1d304d2f3799204ab56b6db15a5ab0c601fdeb10
Alex Mwangi <alex@stl-horizon.com>
2022-03-27 19:14:35 +0300

    COnfigured route for delete-book - calls deleteBook();

--- routes/web.php
+++ routes/web.php
@@ -202,3 +202,4 @@ Route::get('autocomplete', [App\Http\Controllers\ProductController::class, 'auto
 Route::get('books', [App\Http\Controllers\BookController::class, 'index']);
 Route::post('books', [App\Http\Controllers\BookController::class, 'store']);
 Route::get('fetch-books', [App\Http\Controllers\BookController::class, 'fetchBooks']);
+Route::post('delete-book/{id}', [App\Http\Controllers\BookController::class, 'deleteBook']);
