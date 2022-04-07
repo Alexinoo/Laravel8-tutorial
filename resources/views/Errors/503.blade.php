0bf896d0506df94c66f691ccda488f2757630a31
Alex Mwangi <alex@stl-horizon.com>
2022-04-07 22:43:45 +0300

    Added to design the application maintenance mode

---
+++ resources/views/Errors/503.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.master')
+
+@section('title', 'Service Unavailable')
+
+@section('content')
+<section style="padding-top: 100px;">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2 text-center">                
+                <h2>Site is under maintenance</h2>
+                <p>We are currently under maintenamce. We will be back shortly.</p>
+                <p>Thanking You for your patience.</p>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+    @endsection
