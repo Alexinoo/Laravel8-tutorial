7594cad68823c44c62f771d2386706f33e7890b0
Alex Mwangi <alex@stl-horizon.com>
2022-04-09 18:07:03 +0300

    Created Movie model plus migration and seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
+
 {
     /**
      * Seed the application's database.
@@ -13,7 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        // populated 10 records in Users table
+        // \App\Models\User::factory(10)->create();
+        // populated data in students table
         // $this->call([StudentTableSeeder::class]);
+
+        //populate data in movies table
+        $faker = Faker::create();
+        foreach (range(1, 100) as $index) {
+            DB::table('movies')->insert([
+                'title' => $faker->text(40),
+                'description' => $faker->text(300),
+            ]);
+        }
     }
 }
