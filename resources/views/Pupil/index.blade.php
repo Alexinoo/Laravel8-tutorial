553a9c1f73abb3b4d8346e03911be7d4fe3b1ba3
Alex Mwangi <alex@stl-horizon.com>
2022-04-18 12:47:33 +0300

    Added card and some padding

--- resources/views/Pupil/index.blade.php
+++ resources/views/Pupil/index.blade.php
@@ -3,10 +3,18 @@
 @section('title', 'Pupil')
 
 @section('content')
-<section style="padding-top: 50px">
+<section style="padding: 50px">
     <div class="row">
-        <div class="col-md-12">
-            {!!  $dataTable->table() !!}
+        <div class="col-md-12  text-center">
+            <div class="card mt-3">
+                <div class="card-header">
+                    <h5>Pupils Records</h5>
+                </div>
+                <div class="card-body">
+                    
+                    {!!  $dataTable->table() !!}
+                </div>
+            </div>
         </div>
     </div>
 </section>
