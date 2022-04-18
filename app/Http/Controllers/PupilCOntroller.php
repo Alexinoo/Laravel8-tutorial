dc25933cd5677737273e1df0db805b326c4e09e9
Alex Mwangi <alex@stl-horizon.com>
2022-04-18 12:34:02 +0300

    return dataTable->render(Pupil.index) to render index view

--- app/Http/Controllers/PupilCOntroller.php
+++ app/Http/Controllers/PupilCOntroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\DataTables\PupilDataTable;
 
 class PupilController extends Controller
 {
@@ -11,9 +12,9 @@ class PupilController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(PupilDataTable $dataTable)
     {
-        //
+        return $dataTable->render('Pupil.index');
     }
 
     /**
